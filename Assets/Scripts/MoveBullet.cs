a6f4ef99e352b9a10af06f9596db507a5d8e3cc8
Tobias 720s <tobiasalbert97@gmail.com>
2019-03-21 17:25:20 +0100

    towers rotate, shoots bullets in a straight line

---
+++ Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+    private int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = (int) transform.rotation.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime*2, Camera.main.transform);
+        deleteBullet();
+    }
+
+    void deleteBullet()
+    {
+        int x = (int) transform.position.x;
+        int y = (int)transform.position.y;
+
+        if (x > 10 || x < -10 || y > 10 || y < -10) {
+            Destroy(this);
+        }
+    }
+}
