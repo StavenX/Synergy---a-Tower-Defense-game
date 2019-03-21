c48cd2af1d86c1fceaa5c108d804dbf245365dbb
Edvin <edvin_myhren@hotmail.com>
2019-03-21 13:06:33 +0100

    Towers can be upgraded
    - Player has gold
    - Gold will be deducted if placing / upgrading towers

---
+++ Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+    public Text goldLabel;
+    private int gold; 
+
+    private void Start()
+    {
+        Gold = 1000; 
+    }
+
+    public int Gold
+    {
+        get
+        {
+            return gold; 
+        }
+        set
+        {
+            gold = value;
+            goldLabel.GetComponent<Text>().text = "GOLD: " + gold; 
+        }
+    }
+
+}
