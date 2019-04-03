328730b70969111619fabcbb4a2883c5d2493479
Unknown <adrian.nesvik@lyse.net>
2019-04-03 19:02:21 +0200

    Added tower cost + show/hide menu button

--- Assets/Scripts/selectTowerButton.cs
+++ Assets/Scripts/selectTowerButton.cs
@@ -6,6 +6,11 @@ public class selectTowerButton : MonoBehaviour
 {
     public GameObject buttonTower;
 
+    void start() 
+    {
+        
+    }
+
     public void ChangeSelectedTower()
     {
         Debug.Log("Selected " + buttonTower.name);
